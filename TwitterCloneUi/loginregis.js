f3bb471258e794f30fb167e251361850c1a618da
jsphemmnl <ejsibayan158@gmail.com>
2024-02-17 01:05:56 +0800

    a

--- TwitterCloneUi/loginregis.js
+++ TwitterCloneUi/loginregis.js
@@ -1,5 +1,30 @@
 localStorage.removeItem("token");
 
+async function register(username, password) {
+    const regisData = {
+        "username": username,
+        "password": password
+    };
+    try {
+        const res = await fetch('/api/v1/auth/register', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(regisData),
+        });
+        if (res.ok) {
+            const resData = await res.json();
+            console.log(resData);
+        } else {
+            const error = await res.json();
+            console.error(error);
+        }
+    } catch (error) {
+        console.error('Error Signing Up:', error);
+    }
+}
+
 async function logIn() {
     const username = document.getElementById('username-log').value;
     const password = document.getElementById('password-log').value;
