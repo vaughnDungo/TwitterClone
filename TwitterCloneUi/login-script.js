050507b7d8f76687fb482b60681128ee7eaed24d
jsphemmnl <ejsibayan158@gmail.com>
2024-02-17 00:50:03 +0800

    login

---
+++ TwitterCloneUi/login-script.js
@@ -0,0 +1,36 @@
+async function logIn() {
+    const username = document.getElementById('username-log').value;
+    const password = document.getElementById('password-log').value;
+    sendDatatoLS(username, password);
+}
+
+async function sendDatatoLS(username, password) {
+    const res = await fetch("http://localhost:3000/api/v1/auth/login", {
+        method: "POST",
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            'username': username,
+            'password': password
+        })
+    })
+    if (res.ok) {
+        const token = await res.text();
+        localStorage.setItem('token', token);
+        window.location.href = "timeline.html";
+    } else {
+        alert('Incorrect Username/Password.')
+        const error = await res.json();
+        console.error(error);
+    }
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+    const loginButton = document.getElementById("login-button");
+
+    loginButton.addEventListener("click", async function (event) {
+        event.preventDefault(); // Prevent the default form submission
+        await logIn(); // Call the login function when the button is clicked
+    });
+});
